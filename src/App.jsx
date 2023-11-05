7c2bde19405ecb9a35e9b3839d4f2484f609780f
vaidik-bajpai <115713002+vaidik-bajpai@users.noreply.github.com>
2023-11-06 00:18:05 +0530

    Removed unnecessay Images

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
   }
 
   useEffect(() => {
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=9aee14d4fc684e53a35202737233110&q=${city}&days=7&aqi=no&alerts=no`, {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_SOME_KEY}&q=${city}&days=7&aqi=no&alerts=no`, {
       mode: "cors"
     })
       .then((response) => {
